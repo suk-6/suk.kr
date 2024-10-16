bb072d800d72b91d234e6ff9c2373bc1deb767eb
suk-6 <me@suk.kr>
2024-10-16 14:27:39 +0900

    feat: add check password

---
+++ src/edge-config/check.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { Value } from "@/models/value";
+import { get } from "@vercel/edge-config";
+import { redirect } from "next/navigation";
+
+export const checkPassword = async (key: string, password: string) => {
+	let data: Value | undefined;
+
+	try {
+		data = await get(key);
+	} catch (error) {
+		console.error(error);
+	}
+
+	if (data) {
+		if (data.password === password) redirect(data.redirectURL);
+	}
+};
