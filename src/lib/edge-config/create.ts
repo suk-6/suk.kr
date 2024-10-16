16f0845001ee8aa0dfa65ac8e44d9f067d03e8b7
suk-6 <me@suk.kr>
2024-10-16 23:16:36 +0900

    refactor: update createItem function in edge-config

--- src/lib/edge-config/create.ts
+++ src/lib/edge-config/create.ts
@@ -1,11 +1,22 @@
-import { Item } from "@/lib/models/item";
 import { request } from "./request";
+import { Value } from "../models/value";
 
-export const createItem = async (item: Item) =>
+class CreateItem implements Pick<Value, "redirectURL" | "password"> {
+	slug: string = "";
+	redirectURL: string = "";
+	password: string | null = null;
+}
+
+export const createItem = async (item: CreateItem) =>
 	await request([
 		{
 			key: item.slug,
-			value: item.value,
+			value: {
+				redirectURL: item.redirectURL,
+				password: item.password,
+				disabled: false,
+				createdAt: new Date(),
+			},
 			operation: "create",
 		},
 	]);
