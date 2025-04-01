fd6950b2a0ea7d66ac9772608e70af82212a67f2
Woosuk Nam <me@suk.kr>
2025-04-01 10:16:49 +0900

    feat: Create page.tsx

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,18 @@
+export default function Index() {
+    return (
+        <iframe
+            style={{
+                position: "absolute",
+                inset: 0,
+                width: "100%",
+                height: "100%",
+                border: "none",
+                margin: 0,
+                padding: 0,
+                overflow: "hidden",
+                zIndex: 999999,
+            }}
+            src="https://my.surfit.io/w/2105395662"
+        />
+    );
+}
